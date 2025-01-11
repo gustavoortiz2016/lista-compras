0392262d36c6b971d33447ed582b07eb92635937
Arely <esmeralda.ramirez24@itca.edu.sv>
2025-01-10 22:51:20 -0600

    se hicieron cambios en los estilos de login

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,4 +1,6 @@
+import {Button,Form,Alert} from 'react-bootstrap';
 import "./style.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 const PLACEHOLDER = "Password"
 
 const Login = () => {
@@ -6,6 +8,16 @@ const Login = () => {
         <input type="text" value={""} placeholder="User name" style={{ }} /><br />
         <input type="password" value={""} placeholder={PLACEHOLDER} /><br />
         <input className="borde" type="button" value={"Agregar"} />
+        <Button variant="outline-light">Light</Button>
+        <Button variant="warning">Primary</Button>
+        <Form.Control
+        type="password"
+        id="inputPassword5"
+        aria-describedby="passwordHelpBlock"
+      />
+      <Alert variant={"dark"}>
+          This is a  alert—check it out!
+        </Alert>
     </>)
 }
 
