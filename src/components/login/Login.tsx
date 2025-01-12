dfaacdf5ffa36f31cc6e890c9e21c0653b105d66
Arely <esmeralda.ramirez24@itca.edu.sv>
2025-01-11 21:35:18 -0600

    cambios de css en el login

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,23 +1,25 @@
-import {Button,Form,Alert} from 'react-bootstrap';
+import {Button,Form} from 'react-bootstrap';
 import "./style.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 const PLACEHOLDER = "Password"
 
 const Login = () => {
     return (<>
-        <input type="text" value={""} placeholder="User name" style={{ }} /><br />
-        <input type="password" value={""} placeholder={PLACEHOLDER} /><br />
-        <input className="borde" type="button" value={"Agregar"} />
-        <Button variant="outline-light">Light</Button>
-        <Button variant="warning">Primary</Button>
-        <Form.Control
-        type="password"
-        id="inputPassword5"
-        aria-describedby="passwordHelpBlock"
-      />
-      <Alert variant={"dark"}>
-          This is a  alert—check it out!
-        </Alert>
+    <form className='container'>
+        <table className="center"> 
+            <tr>
+                <td>
+                    <input className='one' type="text" value="E-mail/User" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input className='two' type="password" value="passwor" />
+                </td>
+            </tr>
+        </table>
+    </form>
+    
     </>)
 }
 
