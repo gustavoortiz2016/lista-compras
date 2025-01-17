5a258f30d4a137dd71cc06f4c14472f10d7e4b96
Arely <esmeralda.ramirez24@itca.edu.sv>
2025-01-16 18:41:58 -0600

    actua;izacion de rama0

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,15 +1,26 @@
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form ,Alert} from 'react-bootstrap';
 import "./style.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 const PLACEHOLDER = "Password"
 
-const Login = () => {
+interface ILoginProps {
+    username:string
+    password?:string
+    valido?:boolean
+    handleLogin: () => void
+}
+
+const Login:React.FC<ILoginProps>= ({username, password, valido,handleLogin}) => {
     return (<>
         <div>
             <div className='container'>
                 <form className='form-container'>
-                    <input className='one' type="text" placeholder="Username" />
-                    <input className='two' type="password" placeholder="password" />
+                <Form.Control type="email" placeholder="nombre de usuario" defaultValue={username} />
+                <Form.Control type="password" defaultValue={password} />
+                <Button variant="primary">Primary</Button>
+                <Alert variant={"warning"}>
+          This is a alert—check it out!
+        </Alert>
                 </form>
             </div>
         </div>
